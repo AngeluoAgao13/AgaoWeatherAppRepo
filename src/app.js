6a67aa3b7adb8c15f0dd2d436927f9411075d694
AngeluoAgao13 <angeluoagao@gmail.com>
2021-10-19 23:01:43 +0800

    weatherapp1st

---
+++ src/app.js
@@ -0,0 +1,113 @@
+const path = require ('path')
+const express = require ('express')
+const hbs = require ('hbs') // where we are going to see our partials
+const geocode = require ('./utils/geocode')
+const forecast = require ('./utils/forecast')
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))  --->  //to point the public directory
+
+const app = express()
+
+// Define   paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public') //to point the public directory
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine an dviews location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+// this function ay para ma search an app.com or any .com that we creat
+
+
+app.get('', (req, res) => { // this is for the index.html  main page 
+    res.render('index', {
+        title: 'Weather Forecast',
+        name: 'AnggeAngge'
+    })
+})
+
+app.get ('/about', (req, res) => {  //this is for the about page
+    res.render('about', {
+        title: 'About me',
+        name: 'AnggeAngge',
+        aboutMessage: 'This is the group members',
+    })
+})
+
+app.get ('/help',(req, res) =>{
+    res.render('help', {
+        helptext: 'do you wanna build a snowman?',
+        title: 'Help',
+        name: 'Angge Angge'
+    })
+}) 
+app.get('/weather',(req,res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send ({error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send ({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+    // res.send({
+    //     forecast: 'Its summer',
+    //     location: 'Philippines' ,
+    //     address: req.query.address
+    // })
+})
+
+app.get ('/products',(req, res) =>{
+
+    if (!req.query.search) {
+       return res.send({
+            error: 'You must provide a search term'
+        })
+    } 
+
+    console.log(req.query.search)
+    res.send ({
+        products: []
+    })
+})
+app.get('/help/*',(req, res ) => {
+    res.render ('404', {
+        title: '404',
+        name: 'Angge Angge',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get ('*', (req, res) => {
+    res.render ('404', {
+        title: '404',
+        name: 'Angge Angge',
+        errorMessage: 'Sorry! Page not found.'
+    })
+})
+
+
+
+app.listen(3000, () => {
+    console.log("server is up on port 3000.")
+})
---
+++ src/utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require ('request')
+
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address +'.json?access_token=pk.eyJ1IjoiYW5nZ2UtMTMiLCJhIjoiY2t1ZTE5a3h2MWcwcDJvcXBxMm84aXcyYSJ9.xg7dw3AOGlreVcUib80RjA&limit=1'
+
+    request ({ url, json: true }, ( error, { body }) => {
+        if (error) {
+            callback('Unable to connect to the internet', undefined) 
+         } else if  (body.features.length === 0) {
+            callback('Unable to find Location. Try another Search', undefined) 
+       }else {
+           callback(undefined, {
+               latitude: body.features[0].center[0],
+               longitude: body.features[0].center[1],
+               location: body.features[0].place_name
+               
+           })
+       }
+    })
+}     
+
+module.exports = geocode 
